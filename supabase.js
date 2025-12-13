48a48d42d3c7f747a5493a72bcd7d03fde5f34dd
bilalbbh106-hash <bilalbbh106@gmail.com>
2025-12-13 10:41:32 +0100

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
-const SUPABASE_URL = "https://zpsbdkcvdpjzahhvdlvd.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwc2Jka2N2ZHBqemFoaHZkbHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MjI1NTIsImV4cCI6MjA4MTA5ODU1Mn0.r_wsZ3mfaA85Ae6zcGu3Xy61yknt1AyAw5sRpH1tdVA";
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-</script>
